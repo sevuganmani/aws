49c7855648b71bf2970eb8345254257a85a4c627
root <sevugan96mani@gmail.com>
2021-07-27 14:53:22 +0000

    awsuser

---
+++ usercreate.rb
@@ -0,0 +1,8 @@
+# This cookbook is no longer needed to load the aws-sdk and can be removed
+# from node run_lists without any impact
+
+Chef::Log.warn('The default aws recipe does nothing. See the readme for information on using the aws resources')
+aws_iam_user 'vagrantuser' do
+  action :create
+  path '/'
+end
